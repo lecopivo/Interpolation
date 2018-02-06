c89c582d9e3d95775f47fb428386544a43a9c35f
Tomas SKRIVAN <tskrivan@ldchwo008.ist.local>
2018-02-06 15:22:29 +0100

    Reimplemented interpolation in a different way

---
+++ src/Interpolation2.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cmath>
+#include <tuple>
+
+#include "meta_utils.h"
+
+template <class... Interpolation1D> class InterpolationDimWise {
+public:
+  constexpr static int Dim = sizeof...(Interpolation1D);
+
+public:
+  InterpolationDimWise(Interpolation1D... interpolation)
+      : m_interpolation(interpolation...) {}
+
+  template <class Fun, class... Real> auto operator()(Fun &&fun, Real... x) {
+    return interpolate_impl<0>(fun, x...);
+  }
+
+private:
+  template <int I, class Fun, class... Real>
+  inline auto interpolate_impl(Fun &&fun, Real... x) {
+
+    if constexpr (I == Dim) {
+      return fun(x...);
+    } else {
+      auto y = get<I>(x...);
+      auto &interpolation = std::get<I>(m_interpolation);
+      auto funI = [x..., &fun,this](int i) mutable {
+        get<I>(x...) = i;
+        return interpolate_impl<I + 1>(fun, x...);
+      };
+      return interpolation(funI, y);
+    }
+  }
+
+  std::tuple<Interpolation1D...> m_interpolation;
+};
+
+auto ConstantInterpolation = [](auto &&fun, auto x) {
+  int ix = (int)round(x);
+
+  return fun(ix);
+};
+
+// maybe the return type should be:
+// decltype(fun(int())
+// because arithemetics operation can create only an expression(for with Eigen)
+auto LinearInterpolation = [](auto &&fun, auto x) {
+  auto wx = x - floor(x);
+  int ix = (int)floor(x);
+
+  return (wx != 0 ? wx * fun(ix + 1) : 0) + (wx != 1 ? (1 - wx) * fun(ix) : 0);
+
+};
+
+auto CubicInterpolation = [](auto &&fun, auto x) {
+  // for explanation see https://en.wikipedia.org/wiki/Cubic_Hermite_spline
+  int ix = (int)floor(x);
+  auto wx = x - ix;
+
+  auto pm1 = fun(ix - 1);
+  auto p0 = fun(ix);
+  auto p1 = fun(ix + 1);
+  auto p2 = fun(ix + 2);
+
+  auto m0 = 0.5 * (p1 - pm1);
+  auto m1 = 0.5 * (p2 - p0);
+
+  auto t1 = wx;
+  auto t2 = t1 * wx;
+  auto t3 = t2 * wx;
+
+  return (2.0 * t3 - 3.0 * t2 + 1.0) * p0 + (t3 - 2.0 * t2 + t1) * m0 +
+         (-2.0 * t3 + 3.0 * t2) * p1 + (t3 - t2) * m1;
+};
---
+++ tests/sandbox.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+#include "Interpolation2.h"
+
+int main() {
+
+  //  _   ____  _                          _
+  // / | |  _ \(_)_ __ ___   ___ _ __  ___(_) ___  _ __
+  // | | | | | | | '_ ` _ \ / _ \ '_ \/ __| |/ _ \| '_ \ 
+  // | | | |_| | | | | | | |  __/ | | \__ \ | (_) | | | |
+  // |_| |____/|_|_| |_| |_|\___|_| |_|___/_|\___/|_| |_|
+
+  auto fun1d = [](int i) {
+    if (i <= 0)
+      return -1.0;
+    return 1.0;
+  };
+
+  std::cout << "x:\t";
+  for (int i = 0; i <= 10; i++) {
+    double x = i / 10.0;
+    std::cout << x << "\t";
+  }
+
+  std::cout << std::endl <<  "Const:\t";
+  for (int i = 0; i <= 10; i++) {
+    double x = i / 10.0;
+    std::cout << ConstantInterpolation(fun1d, x) << "\t";
+  }
+
+  std::cout << std::endl <<  "Linear:\t";
+  for (int i = 0; i <= 10; i++) {
+    double x = i / 10.0;
+    std::cout << LinearInterpolation(fun1d,x) << "\t";
+  }
+
+  std::cout << std::endl <<  "Cubic:\t";
+  for (int i = 0; i <= 10; i++) {
+    double x = i / 10.0;
+    std::cout << CubicInterpolation(fun1d,x) << "\t";
+  }
+
+  //  ____    ____  _                          _
+  // |___ \  |  _ \(_)_ __ ___   ___ _ __  ___(_) ___  _ __  ___
+  //   __) | | | | | | '_ ` _ \ / _ \ '_ \/ __| |/ _ \| '_ \/ __|
+  //  / __/  | |_| | | | | | | |  __/ | | \__ \ | (_) | | | \__ \
+  // |_____| |____/|_|_| |_| |_|\___|_| |_|___/_|\___/|_| |_|___/
+  
+  std::cout << std::endl << std::endl << "Linear interpolation in 2D: " << std::endl;
+
+  auto interpol2d =
+      InterpolationDimWise(LinearInterpolation, LinearInterpolation);
+
+  auto fun2d = [](int i, int j) {
+    if (i <= 0 || j <= 0)
+      return -1.0;
+    return 1.0;
+  };
+
+  for (int i = 0; i <= 10; i++) {
+    for (int j = 0; j <= 10; j++) {
+      double x = i * 0.1;
+      double y = j * 0.1;
+
+      std::cout << interpol2d(fun2d, x, y) << "\t";
+    }
+    std::cout << std::endl;
+  }
+
+  //                 _                    _       _                        _
+  //   ___ _   _ ___| |_ ___  _ __ ___   (_)_ __ | |_ ___ _ __ _ __   ___ | |
+  //  / __| | | / __| __/ _ \| '_ ` _ \  | | '_ \| __/ _ \ '__| '_ \ / _ \| |
+  // | (__| |_| \__ \ || (_) | | | | | | | | | | | ||  __/ |  | |_) | (_) | |
+  //  \___|\__,_|___/\__\___/|_| |_| |_| |_|_| |_|\__\___|_|  | .__/ \___/|_|
+  //                                                          |_|
+
+  std::cout << std::endl << "Custom Interpolation: " << std::endl;
+
+  auto CustomInterpolation = [](auto &&fun, auto x) {
+    auto wx = x - floor(x);
+    int ix = (int)floor(x);
+
+    if (wx >= 0.3 && wx <= 0.7)
+      return 0.0;
+
+    return (wx != 0 ? wx * fun(ix + 1) : 0) +
+           (wx != 1 ? (1 - wx) * fun(ix) : 0);
+  };
+
+  auto custom_interpol =
+      InterpolationDimWise(CustomInterpolation, CustomInterpolation);
+
+  for (int i = 0; i <= 10; i++) {
+    for (int j = 0; j <= 10; j++) {
+      double x = i * 0.1;
+      double y = j * 0.1;
+
+      std::cout << custom_interpol(fun2d, x, y) << "\t\e[0m";
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
